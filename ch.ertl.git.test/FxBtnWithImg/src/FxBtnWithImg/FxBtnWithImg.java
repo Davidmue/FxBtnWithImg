7e137a1b6ae13e4c34c423f2b5de2f727351546a
Davidmue <d.mueller@ertl.ch>
2015-05-01 15:47:26 +0200

    Label size effect added, Window size changed!

--- ch.ertl.git.test/FxBtnWithImg/src/FxBtnWithImg/FxBtnWithImg.java
+++ ch.ertl.git.test/FxBtnWithImg/src/FxBtnWithImg/FxBtnWithImg.java
@@ -9,6 +9,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class FxBtnWithImg extends Application {
@@ -20,18 +21,23 @@ public class FxBtnWithImg extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
 		stage.setTitle("A button with an image");
+		stage.setWidth(200);
+		stage.setHeight(200);
 		BorderPane root = new BorderPane();
 		Label lbl = new Label("Ready!");
 		Image img = new Image(getClass().getResourceAsStream("ok.png")); 
 		Button btn = new Button("", new ImageView(img));
 		btn.setOnMouseEntered((MouseEvent e) -> {
 			lbl.setText("Mouse enter!");
+			lbl.setFont(Font.font(12));
 		});
 		btn.setOnMouseExited((MouseEvent e) -> {
 			lbl.setText("Mouse exit!");
+			lbl.setFont(Font.font(12));
 		});
 		btn.setOnAction((ActionEvent e) -> {
-			lbl.setText("Mouse Clicked!");
+			lbl.setText("Mouse Clicked!"); 
+			lbl.setFont(Font.font(14));
 		});
 		root.setCenter(btn);
 		root.setBottom(lbl);
